63306f00387b2da5629967d8a52c20ebc49589ec
Akshay Sarman <39640391+Aryan108@users.noreply.github.com>
2022-04-08 15:57:53 +0530

    Added bitstripping module

--- compressor.v
+++ compressor.v
@@ -1,192 +1,86 @@
 `timescale 1ns / 1ps
 
 module compressor #(
-    parameter    INPUT_WIDTH  = 128,
-    parameter    OUTPUT_WIDTH = 128,
-    parameter CHUNK_SIZE = 8,
-    parameter EN_BITS = 3
-) (
-    input                           clk_in,is_head, // clock, signals from control logic
-    input  [INPUT_WIDTH - 1 : 0]    data_in, // input flit
-    output reg [OUTPUT_WIDTH - 1 : 0]   data_out, head_buffer // compressed flit
-);
+        parameter   INPUT_WIDTH  = 128,
+        parameter   OUTPUT_WIDTH = 128,
+        parameter   CHUNK_SIZE = 8,
+        parameter   EN_BITS = 3,
+        parameter   NUM_CHUNKS = INPUT_WIDTH / CHUNK_SIZE
+    ) (
+        input                               clk_in, is_head, // clock, signals from control logic
+        input  [INPUT_WIDTH - 1 : 0]        data_in, // input flit
+        output [OUTPUT_WIDTH - 1 : 0]       data_out, // compressed flit
+        output [EN_BITS - 1 : 0]            en_out // encoding bits for each flit
+    );
+    wire [INPUT_WIDTH - 1 : 0]  delta_out;
+    wire [CHUNK_SIZE - 1 : 0] Clarge, Csmall, sb0_out, mux_out, base;
+    wire [EN_BITS - 1 : 0] en_bits;
+    wire sel_bit;
 
-wire [CHUNK_SIZE - 1 : 0] Clarge,Csmall,sb0_out,mux_out;
-reg [CHUNK_SIZE - 1 : 0] c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16; // chunks
-wire [CHUNK_SIZE - 1 : 0] d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,base; // deltas
-wire [EN_BITS - 1 : 0] en_bits;
-wire sel_bit;
-integer count = 74; // meta data starting location
-// debug values
-//assign data_in = 128'hA47642BB000000000000000000000000;
 
-assign base = (Clarge + Csmall)/2; // Base flit
+    assign base = (Clarge + Csmall)/2; // Base flit
+    assign en_out = en_bits; // Encode bits
 
-always @(posedge clk_in) begin
-    if (is_head == 1'b1) begin
-         // if flit type is head, then no output for compressor
-        data_out <= 128'bz; // high impedance
-        head_buffer <= data_in; // store head flit for adding metadata
-    end
+    // if (is_head == 1'b1) begin
+    //     // if flit type is head, then no output for compressor
+    //     assign data_out = 128'bz; // high impedance
+    //     //head_buffer <= data_in; // store head flit for adding metadata
+    // end
 
-    else if(is_head == 1'b0) begin
-        {c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16} <= data_in; // chunking of flit
+    // else if(is_head == 1'b0) begin
 
-        
+    //     if (en_bits == 3'b000)
+    //         assign data_out = 128'bz; // if all chunks are same, high impedence
+    // end
 
-        head_buffer[count-:11] <= {en_bits,base}; // add metadata to head flit
+    minmax minmx (
+               .input_data(data_in),
+               .max_data(Clarge),
+               .min_data(Csmall)
+           );
 
-        if (en_bits == 3'b000) begin
-            data_out <= 128'bz; // if all chunks are same, high impedence
+    subtractor sb0 (
+                   .a(base),
+                   .b(Clarge),
+                   .data_out(sb0_out)
+               ); // Subtract the maximum value from the base value
+
+    priority_encoder p_enc(
+                         .data_in(sb0_out),
+                         .code_out(en_bits)
+                     ); // Encode the difference between the base and maximum values
+
+    small_comb comb_logic(
+                   .data_in(en_bits),
+                   .data_out(sel_bit)
+
+               ); // Generate the select bit to be sent to the MUX
+
+    MUX_2_1 vec_mux(
+                .b(base),
+                .a(8'b00000000),
+                .out(mux_out),
+                .sel(sel_bit)
+            ); // MUX to select the correct value to be sent to the subtractor
+
+    bitstrip bs(
+                 .en_bits(en_bits), // add the sign bit
+                 .deltas_in(delta_out),
+                 .data_out(data_out)
+             ); // Removes extra bits from the data
+
+    // Parallel Subtractors
+    genvar i;
+
+    generate // generate the parallel subtractors
+        for(i = 0; i < NUM_CHUNKS; i = i+1 ) begin
+            subtractor SUBi(
+                           .a(data_in[i*CHUNK_SIZE +: CHUNK_SIZE]),
+                           .b(mux_out),
+                           .data_out(delta_out[i*CHUNK_SIZE +: CHUNK_SIZE])
+                       );
         end
-
-        else begin
-            data_out <= {d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16};
-            // else, output deltas
-        end
-    end
-end
-
-
-
-
-CompMAX max(
-    .clk_in(clk_in),
-    .data_in(data_in),
-    .data_out(Clarge)
-); // Compute the maximum value of the input data
-
-CompMIN min(
-    .clk_in(clk_in),
-    .data_in(data_in),
-    .data_out(Csmall)
-); // Compute the minimum value of the input data
-
-
-subtractor sb0 (
-    .a(base),
-    .b(Clarge),
-    .data_out(sb0_out)
-); // Subtract the maximum value from the base value
-
-priority_encoder p_enc(
-    .data_in(sb0_out),
-    .code_out(en_bits)
-); // Encode the difference between the base and maximum values
-
-small_comb comb_logic(
-    .data_in(en_bits),
-    .data_out(sel_bit)
-
-); // Generate the select bit to be sent to the MUX
-
-MUX_2_1 vec_mux(
-    .b(base),
-    .a(8'b00000000),
-    .out(mux_out),
-    .sel(sel_bit)
-); // MUX to select the correct value to be sent to the subtractor
-
-
-// Parallel Subtractors
-
-subtractor sb1(
-    .a(c1),
-    .b(mux_out),
-    .data_out(d1)
-);
-
-subtractor sb2(
-    .a(c2),
-    .b(mux_out),
-    .data_out(d2)
-);
-
-subtractor sb3(
-    .a(c3),
-    .b(mux_out),
-    .data_out(d3)
-);
-
-subtractor sb4(
-    .a(c4),
-    .b(mux_out),
-    .data_out(d4)
-);
-
-subtractor sb5(
-    .a(c5),
-    .b(mux_out),
-    .data_out(d5)
-);
-
-subtractor sb6(
-    .a(c6),
-    .b(mux_out),
-    .data_out(d6)
-);
-
-subtractor sb7(
-    .a(c7),
-    .b(mux_out),
-    .data_out(d7)
-);
-
-subtractor sb8(
-    .a(c8),
-    .b(mux_out),
-    .data_out(d8)
-);
-
-subtractor sb9(
-    .a(c9),
-    .b(mux_out),
-    .data_out(d9)
-);
-
-subtractor sb10(
-    .a(c10),
-    .b(mux_out),
-    .data_out(d10)
-);
-
-subtractor sb11(
-    .a(c11),
-    .b(mux_out),
-    .data_out(d11)
-);
-
-subtractor sb12(
-    .a(c12),
-    .b(mux_out),
-    .data_out(d12)
-);
-
-subtractor sb13(
-    .a(c13),
-    .b(mux_out),
-    .data_out(d13)
-);
-
-subtractor sb14(
-    .a(c14),
-    .b(mux_out),
-    .data_out(d14)
-);
-
-subtractor sb15(
-    .a(c15),
-    .b(mux_out),
-    .data_out(d15)
-); 
-
-subtractor sb16(
-    .a(c16),
-    .b(mux_out),
-    .data_out(d16)
-);
-// End of Parallel Subtractors
-
+    endgenerate
 
 
 endmodule  //compressor
--- testbench.v
+++ testbench.v
@@ -1,60 +1,58 @@
 `timescale 1ns / 1ps
 
-module general_tb ();
+module general_tb #(
+        parameter N_FLITS = 10000
 
-reg clk,is_head,is_tail;
-reg [127:0] in;
-wire [127:0] out,head_buffer;
-reg [127: 0] ram [4 : 0];
+    )();
 
-integer f;
+    reg clk;
+    reg [127 : 0] in;
+    wire [127 : 0] comp_out;
+    wire [7 : 0] count;
+    wire [2:0] en_bits;
+    // reg [127: 0] ram [4 : 0];
 
-compressor DUT(
-    .clk_in(clk),
-    .data_in(in),
-    .data_out(out),
-    .is_head(is_head),
-    .is_tail(is_tail),
-    .head_buffer(head_buffer)
+    integer i=0;
+    real compr_ratio=0, avg_ratio=0;
 
-);
+    compressor compr(
+                   .clk_in(clk),
+                   .data_in(in),
+                   .data_out(comp_out),
+                   .en_out(en_bits),
+                   .is_head(1'b0)
+               );
 
-always #5
-    clk = ~clk; // flip clk every 5ns
+    en_table en_tab(
+                 .en_bits(en_bits),
+                 .count(count) // compressed data count
+             );
 
-always @(out)
-        $fwrite(f,"%h\n",out);
+    always #5
+        clk = ~clk; // flip clk every 5ns
 
+
+    always #30 begin
+        in <= {{$random}, {$random}, {$random}, {$random}}; // typical base flit
+        #20
+         $display("[DATA LOG] flit %0d - input=0x%H    compressed=0x%H    en_bits=%b    compressed-size=%0d", i, in, comp_out, en_bits, count);
+        compr_ratio = (128 - count)/128.0;
+
+
+
+        if (i == N_FLITS) begin
+            $display("[TEST RESULT] Average compression ratio=%0.2f%%", 100*(avg_ratio)/N_FLITS);
+            $stop;
+        end
+        i = i + 1;
+        avg_ratio = avg_ratio + compr_ratio;
+
+    end
 
     initial begin
-        clk <= 1;
-        f = $fopen("D:/FPGA/projects/Flitzip/deltas.txt","w");
-        
-        $readmemh("D:/FPGA/projects/Flitzip/flits.txt",ram); // read from file
+        clk <= 0;
 
-        is_head <= 1;
-        is_tail <= 0;
-
-        #50 in <= ram[0];
-        
-        #50 in <= ram[1];
-        is_head <= 0;
-
-        #50 in <= ram[2];
-
-        #50 in <= ram[3];
-
-        #50 in <= ram[4];
-        is_tail <= 1;
-        is_head <= 0;
-
-
-        #50 $fwrite(f,"%b\n",head_buffer);
-        $fclose(f);
-
-        
-        $stop;
-    
     end
 
 endmodule  //general_tb
+
