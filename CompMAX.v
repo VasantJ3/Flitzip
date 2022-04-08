a776c55831545860ca62ebe7e8619e0d221c4559
Akshay Sarman <39640391+Aryan108@users.noreply.github.com>
2022-04-08 15:54:06 +0530

    Modified minmax modules

--- CompMAX.v
+++ CompMAX.v
@@ -1,29 +1,13 @@
 module CompMAX #(
-    parameter    INPUT_WIDTH  = 128,
-    parameter    OUTPUT_WIDTH = 8,
-    parameter N = 128, // number of chunks
-    parameter D = 8 // size of each chunk
-) (
-    input                           clk_in,
-    input   [INPUT_WIDTH - 1 : 0]    data_in,
-    output  [OUTPUT_WIDTH - 1 : 0]   data_out
-);
-reg [D - 1 : 0]    temp; // temp register to hold data
-integer j;
+        parameter    INPUT_WIDTH  = 8,
+        parameter    OUTPUT_WIDTH = 8
+    ) (
+        input  [INPUT_WIDTH - 1 : 0]    in_a,in_b,
+        output [OUTPUT_WIDTH - 1 : 0]   data_out
+    );
+
+    assign data_out = in_a > in_b ? in_a : in_b;
 
 
+endmodule  //module_name
 
-always @(posedge clk_in ) begin
-    j=0;
-
-    temp <= data_in[j+:8]; // load data into temp
-    for(j=8;j<N;j=j+8) begin // loop over next chunks
-        if(temp <= data_in[j+:8]) // if temp is smaller than next chunk
-            temp <= data_in[j+:8]; // load next chunk into temp
-    end
-
-    
-end
-assign data_out = temp; // output the maximum
-
-endmodule  //CompMAX
